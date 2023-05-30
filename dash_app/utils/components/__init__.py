ec61f726f2e7c89b3fae7c0701763e55608779da
Gioele-M <gioelemontis97@gmail.com>
2023-05-30 11:19:14 +0100

    second round of stripping

--- dash_app/utils/components/__init__.py
+++ dash_app/utils/components/__init__.py
@@ -7,7 +7,3 @@ from .ClickablePlateSelectionAIO import ClickablePlateSelectionAIO
 from .CollapsableCardWrapperAIO import CollapsableCardWrapperAIO
 from .DataTableDisplay96WellAIO import DataTableDisplay96WellAIO
 from .PageTabsAIO import PageTabsAIO
-from .DataTableAIO import DataTableAIO
-from .redis_store import redis_store
-from .SampleSelectorAIO import SampleSelectorAIO
-from .GraphXYSelectorAIO import GraphXYSelectorAIO
--- dash_app/utils/components/qc_subpages/__init__.py
+++
@@ -1,3 +0,0 @@
-from .upload_page import upload_page_layout
-from .line_plot import line_plot_layout
-from .bar_plot import bar_plot_layout
