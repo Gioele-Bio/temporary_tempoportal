b2f0e2e2bc74033a8c35124a769c269bd2e52170
Gioele-M <gioelemontis97@gmail.com>
2023-05-30 16:10:19 +0100

    explicit server

--- src/app.py
+++ src/app.py
@@ -1,9 +1,9 @@
 from dash_app import create_dash_application
 
 # Run App
-if __name__ == '__main__':
-    app = create_dash_application(__name__, local=True)
 
-    server = app.server
+app = create_dash_application(__name__, local=True)
 
-    app.run_server(port=9000, debug=True) 
+server = app.server
+
+#app.run_server(port=9000, debug=True) 
